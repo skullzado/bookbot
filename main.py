50b63ab65c7ccf74b9f85e8b7d88a63dbf692d15
skullzado <pafcalzado@gmail.com>
2023-11-10 13:06:52 +0800

    add function to print report for book info

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ def count_letters(text):
         else:
             letter_count[char] = 1
     return letter_count
+
+
+# Print a report
+def print_a_report(text):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(text)} words found in the document\n")
+
+    letter_count = list(count_letters(text).items())
+    letter_count.sort()
+    for item in letter_count:
+        if item[0].isalpha():
+            print(
+                f"The '{item[0]}' character was found {item[1]} times")
+
+    print("\n--- End report ---")
