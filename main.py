35b248823794779af53a217e4703ecce55aa7731
Paul Calzado <pafcalzado@gmail.com>
2024-01-25 19:29:34 +0800

    Update main.py to match guided proj's solution

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
-# Count all words
-def count_words(text):
-    return len(text.split())
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
-# Count all letters
-def count_letters(text):
-    letter_count = {}
-    text = text.lower()
-    for char in text:
-        if char in letter_count:
-            letter_count[char] += 1
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            letter_count[char] = 1
-    return letter_count
+            chars[lowered] = 1
+    return chars
 
 
-# Print a report
-def print_a_report(text):
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{count_words(text)} words found in the document\n")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-    letter_count = list(count_letters(text).items())
-    letter_count.sort()
-    for item in letter_count:
-        if item[0].isalpha():
-            print(
-                f"The '{item[0]}' character was found {item[1]} times")
 
-    print("\n--- End report ---")
+main()
